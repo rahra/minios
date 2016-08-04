15ab5ce15f00abdaa589caf987f518b926f3200f
eagle <eagle@58e1ccc2-750e-0410-8d0d-f93ca75ab447>
2016-08-04 21:01:40 +0000

    improved code comments

--- src/ledproc.c
+++ src/ledproc.c
@@ -1,18 +1,31 @@
+/*! \file ledproc.c
+ * This file contains the function led_proc() which continuously toggles the
+ * led on the Arduino board. The function is started as separate process by
+ * main().
+ * The function wait() waits a given number of time slices.
+ * @author Bernhard R. Fischer, 4096R/8E24F29D bf@abenteuerland.at
+ */
 
-int toggle_led(void);
-unsigned long get_uptime(void);
+int toggle_led(void);            // defined in led.S
+unsigned long get_uptime(void);  // defined in timer.S
+void sys_sleep(void);            // defined in process.S
 
 
+/* Waits the defined number t of time slices until it returns.
+ * @param t Number of time slices to wait.
+ */
 void wait(unsigned int t)
 {
    unsigned long end;
 
    end = get_uptime() + t;
-   while (get_uptime() < end);
+   while (get_uptime() < end)
+      sys_sleep();
 }
 
 
-
+/* This function continuously toggles the led. This function does never return.
+ */
 void led_proc(void)
 {
    for (;;)
@@ -21,5 +34,4 @@ void led_proc(void)
       wait(5);
    }
 }
-
 
